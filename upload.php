697a6f497dba958b499c4c499061cfb1c9a3de37
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-10-21 09:41:29 -0400

    added with indentation

--- upload.php
+++ upload.php
@@ -1,31 +1,31 @@
-<?php
- $obj = new upload();
+ <?php
+  $obj = new upload();
+  
+  class upload
+  {
+  
+     public function __construct()
+     {
+         echo "in upload";
+          if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $this::post();
+          }
+     }
+     
+     public static function post()
+     {
+     //static $myvar = 45;
+  $target_dir = "upload_dir/";
  
- class upload
- {
- 
-    public function __construct()
-    {
-        echo "in upload";
-         if($_SERVER['REQUEST_METHOD'] == 'POST') {
-           $this::post();
-         }
-    }
-    
-    public static function post()
-    {
-    //static $myvar = 45;
- $target_dir = "upload_dir/";
-
-$file_ext=strtolower(end(explode('.',$_FILES["fileToUpload"]["name"])));
+ $file_ext=strtolower(end(explode('.',$_FILES["fileToUpload"]["name"])));
  $tmp_name = $_FILES["fileToUpload"]["tmp_name"];
  $name = basename($_FILES["fileToUpload"]["name"]);
-move_uploaded_file($tmp_name, "$target_dir/$name");
+ move_uploaded_file($tmp_name, "$target_dir/$name");
  $sow = $_FILES["fileToUpload"]["name"];
-$s = $target_dir . $sow ;
+ $s = $target_dir . $sow ;
  $t = "upload_dir/" . $_FILES["fileToUpload"]["name"]; 
-header("Location: csv2html.php?file=$t");
-}
-
-}
-?>
+ header("Location: csv2html.php?file=$t");
+ }
+ 
+ }
+ ?>
