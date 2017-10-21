95d449d6e04a024dacb5e331879b776b158e641a
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-10-21 00:22:47 -0400

    updated index file

--- index.php
+++ index.php
@@ -1,22 +1,51 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
+<?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-<body>
 
-<form action="upload.php" method="post" enctype="multipart/form-data">
-      BROWSE THE FILE TO UPLOAD: <br><br>
-    <input type="file" name="fileToUpload" id="fileToUpload">
+class Manage {
+    public static function autoload($class) {
+        include $class . '.php';
+        echo "rrr";
+    
+    }
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+$obj = new main();
+echo $obj::$html;
+//$obj1 = new csv2html();
+//echo $obj::post();
+
+class main
+{
+public static $html;
+
+public  function __construct()
+{
+$form =   '<!DOCTYPE html>';
+$form .=   '<html>';
+$form .=   '<head>';
+$form .=   '<style>.error {color: #FF0000;}';
+$form .=   '<link rel="stylesheet" href="styles.css">';
+$form .=   '</style></head><body>';
+
+$form .=   '<form action="upload.php" method="post" enctype="multipart/form-data">';
+$form .=   ' BROWSE THE FILE TO UPLOAD: <br><br>';
+   $form .=   ' <input type="file" name="fileToUpload" id="fileToUpload">';
  
-    <span class="error">* <?php echo $err;?></span>
-  <br><br>
-   <br><br> <input type="submit" value="Upload File" name="submit">
-</form>
+  $form .=   '<br><br>';
+  $form .=   ' <br><br> <input type="submit" value="Upload File" name="submit">';
+$form .=   '</form>';
 
-</body>
-</html>
+ $form .=   '</body>';
+$form .=   '</html>';
+$this::$html .= $form;
+}
+}
 
+
+
+
+?>
