7fb9c20af77899c52c8168f8e31a670c7b4108ec
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-10-21 09:41:15 -0400

    added with indentation

--- csv2html.php
+++ csv2html.php
@@ -1,47 +1,51 @@
 <?php
-
-$obj = new csv2html();
-
-
-class csv2html {
-
-public function __construct()
-    {
-        echo "in upload";
+ 
+ $obj = new csv2html();
+ 
+ 
+ 
+ 
+ 
+ class csv2html {
+ 
+ public function __construct()
+     {
+        
+          
+            $this::csv();
          
-           $this::csv();
-        
-    }
-
-public static function csv(){
+     }
+ 
+ public static function csv(){
+ 
  $sow  = $_REQUEST["file"];
-echo '<html>';
-echo '<head>';
-echo '<link rel="stylesheet" href="style.css">';
-echo '</head>';
-echo '<body>';
-$row = 1;
-if (($handle = fopen($sow, "r")) !== FALSE) {
-echo '<table>';
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-       $num = count($data);
-       echo '<tr>';
-        //echo "<p> $num fields in line $row: <br /></p>\n";
-        
-        for ($c=0; $c < $num; $c++) {
-           if ($row == 0)
-            echo '<th>' . $data[$c] . '</th>';
-            else echo '<td>' .  $data[$c] . '</td>';
-        }
-    echo '</tr>';
-    $row++;
-    }
-    echo '</table>';
-    fclose($handle);
-    }
-    
-    echo '</body>';
-    echo '</html>';
-    }
-}
-?>
+ echo '<html>';
+ echo '<head>';
+ echo '<link rel="stylesheet" href="style.css">';
+ echo '</head>';
+ echo '<body>';
+ 
+ $row = 1;
+ if (($handle = fopen($sow, "r")) !== FALSE) {
+ echo '<table>';
+     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo '<tr>';
+         //echo "<p> $num fields in line $row: <br /></p>\n";
+         
+         for ($c=0; $c < $num; $c++) {
+            if ($row == 1) echo '<th>' . $data[$c] . '</th>';
+            else  echo '<td>' .  $data[$c] . '</td>';
+         }
+     echo '</tr>';
+     $row++;
+     }
+     echo '</table>';
+     fclose($handle);
+     }
+     
+     echo '</body>';
+     echo '</html>';
+     }
+ }
+ ?>
