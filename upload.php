fc12af669a8fa7e9457b5fc7289d0926440453c6
Sowmyasundari Nellore <sn478@afsconnect1.njit.edu>
2017-10-22 11:29:32 -0400

    added edited upload

--- upload.php
+++ upload.php
@@ -6,7 +6,7 @@
   
      public function __construct()
      {
-         echo "in upload";
+         
           if($_SERVER['REQUEST_METHOD'] == 'POST') {
             $this::post();
           }
@@ -14,10 +14,10 @@
      
      public static function post()
      {
-     //static $myvar = 45;
+     
   $target_dir = "upload_dir/";
  
- $file_ext=strtolower(end(explode('.',$_FILES["fileToUpload"]["name"])));
+ 
  $tmp_name = $_FILES["fileToUpload"]["tmp_name"];
  $name = basename($_FILES["fileToUpload"]["name"]);
  move_uploaded_file($tmp_name, "$target_dir/$name");
